e2e2cf11d97253d77a3bf9864a0982fb280062f1
fuyusakaiori <670232228@qq.com>
2022-02-17 16:07:49 +0800

    合并链表系列问题

--- src/main/java/chapter02/MergeLists.java
+++ src/main/java/chapter02/MergeLists.java
@@ -16,9 +16,7 @@ public class MergeLists
     }
 
     /**
-     * <p>合并两个有序链表</p>
-     * @param list1 第一个链表
-     * @param list2 第二个链表
+     * <h3>思路: 哑元 + 比大小</h3>
      */
     private static ListNode mergeTwoSortedLists(ListNode list1, ListNode list2){
         ListNode dummy = new ListNode(0);
@@ -33,42 +31,45 @@ public class MergeLists
             }
             current = current.next;
         }
+        if (list1 != null) current.next = list1;
+        if (list2 != null) current.next = list2;
         return dummy.next;
     }
 
 
     /**
-     * 合并两个链表
-     * @param left 指定合并的区域的左边界
-     * @param right 指定的合并的取悦右边界
-     * @return 新的链表
+     * <h3>思路: 找到合并链表的起始位置和终止位置就行</h3>
      */
     private static ListNode mergeBetweenLists(ListNode list1, ListNode list2, int left, int right){
+        ListNode head = list1;
+        ListNode previous = null;
+        ListNode next = null;
         int index = 0;
-        ListNode current = list1;
-        ListNode leftNode = null;
-        ListNode rightNode = null;
-        while (current != null && index <= right){
-            leftNode = index == left - 1 ? current: leftNode;
-            rightNode = index == right ? current: rightNode;
+        // 先找到合并链表的起始位置
+        while(index < left - 1){
+            list1 = list1.next;
             index++;
-            current = current.next;
         }
-        current = list2;
-        while (current.next != null)
-            current = current.next;
-
-        if (leftNode != null)
-            leftNode.next = list2;
-        current.next = rightNode;
-
-        return list1;
+        // 记录起始结点
+        previous = list1;
+        // 找到合并链表为结束位置
+        while(index < right + 1){
+            list1 = list1.next;
+            index++;
+        }
+        // 找到第二个链表的尾部
+        while(list2.next != null) list2 = list2.next;
+        next = list1;
+        // 开始合并链表
+        previous.next = list2;
+        list2.next = next;
+        return head;
     }
 
     /**
-     * <p>合并 K 个升序链表</p>
-     * <p>我之前的想法, 应该是遍历链表, 两两合并</p>
-     * <p>但仔细想想, 显然采用归并的方式去两两合并, 时间复杂度是更低的</p>
+     * <h3> 思路: </3>
+     * <h3>1. 遍历链表数组, 两个链表先合并得到排序的链表, 然后生成链表再和之后的链表合并, 时间复杂度为 O(n^2)</h3>
+     * <h3>2. 遍历链表数组, 但是两两合并, 合并的结果再两两合并, 类似于归并排序</h3>
      * @param lists 链表数组
      * @return 新的链表
      */
@@ -81,24 +82,10 @@ public class MergeLists
         if (left == right)
             return lists[left];
         int mid = left + ((right - left) >> 1);
-        ListNode list1 = fork(lists, left, mid);
-        ListNode list2 = fork(lists, mid + 1, right);
-        return merge(list1, list2);
+        ListNode leftList = fork(lists, left, mid);
+        ListNode rightList = fork(lists, mid + 1, right);
+        return mergeTwoSortedLists(leftList, rightList);
     }
 
-    private static ListNode merge( ListNode list1, ListNode list2){
-        ListNode dummy = new ListNode(0);
-        ListNode current = dummy;
-        while (list1 != null && list2 != null){
-            if (list1.value < list2.value){
-                current.next = list1;
-                list1 = list1.next;
-            }else{
-                current.next = list2;
-                list2 = list2.next;
-            }
-            current = current.next;
-        }
-        return dummy.next;
-    }
+
 }

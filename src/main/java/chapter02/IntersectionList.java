fb418a85b618df8e89f444ef3aa9d912b45b9e5f
fuyusakaiori <670232228@qq.com>
2022-02-17 16:51:56 +0800

    相交链表系列问题

--- src/main/java/chapter02/IntersectionList.java
+++ src/main/java/chapter02/IntersectionList.java
@@ -6,8 +6,8 @@ import java.util.*;
 
 /**
  * <h2>链表相交</h2>
- * <p>1. 两个链表中都不存在环, 请问是否相交</p>
- * <p>2. 两个链表中不确定是否存在环, 请问是否相交</p>
+ * <h3>1. 相交链表: 链表中没有环, 判断是否相交</h3>
+ * <h3>2. 相交链表 II: 链表存在环, 判断是否相交</h3>
  */
 public class IntersectionList
 {
@@ -17,71 +17,83 @@ public class IntersectionList
 
 
     /**
-     * 采用哈希表完成
-     * @param l1 给定的第一个链表
-     * @param l2 给定的第二个链表
-     * @param noLoop 没有环
-     * @return 相交的结点
+     * <h3>思路: 哈希表</h3>
      */
-    private static ListNode intersectionListHash(ListNode l1, ListNode l2, String noLoop){
-        ListNode current = l1;
+    private static ListNode getIntersectionNodeHash(ListNode l1, ListNode l2){
         Set<ListNode> set = new HashSet<>();
-        // 1. 遍历其中一个链表, 将所有结点添加进哈希表中
-        while (current != null){
-            set.add(current);
-            current = current.next;
+        // 将第一个链表的结点全部存放在哈希表中
+        while(l1 != null){
+            set.add(l1);
+            l1 = l1.next;
         }
-        // 2. 遍历第二个链表, 检查是否有结点在哈希表中
-        current = l2;
-        while (current != null){
-            if (set.contains(current))
-                return current;
-            current = current.next;
+        // 开始检验是否有重复的结点
+        while(l2 != null){
+            if(set.contains(l2))
+                return l2;
+            l2 = l2.next;
         }
         return null;
     }
 
     /**
-     * <p>只使用临时变量完成</p>
+     * <h3>思路: 先计算两个链表的长度, 然后让长的链表先走差值, 最后一起开始移动</h3>
      */
-    private static ListNode intersectionList(ListNode l1, ListNode l2, String noLoop){
-        if (l1 == null || l2 == null)
-            return null;
+    private static ListNode getIntersectionNode(ListNode l1, ListNode l2){
+        int firstLength = getLength(l1);
+        int secondLength = getLength(l2);
+        ListNode longer = firstLength < secondLength ? l2: l1;
+        ListNode shorter = firstLength < secondLength ? l1: l2;
+        int distance = Math.abs(firstLength - secondLength);
+        while (distance-- > 0){
+            longer = longer.next;
+        }
+        while (longer != null && shorter != null){
+            if (longer == shorter)
+                return longer;
+            longer = longer.next;
+            shorter = shorter.next;
+        }
+        return null;
+    }
+
+    private static int getLength(ListNode list){
         int length = 0;
-        // 1. 计算两个链表相差的长度
-        ListNode current = l1;
-        while (current != null){
-            length++;
-            current = current.next;
+        while (list != null && ++length > 0){
+            list = list.next;
         }
-        current = l2;
-        while (current != null){
-            length--;
-            current = current.next;
-        }
-        // 2. 长的链表先走多出来的部分
-        int index = Math.abs(length);
-        if (length > 0){
-            while (index-- > 0)l1 = l1.next;
-        }else{
-            while (index-- > 0)l2 = l2.next;
-        }
-        // 3. 两个链表一起走, 相等的时候停止
-        while (l1 != l2){
-            l1 = l1.next;
-            l2 = l2.next;
-        }
-        return l1;
+        return length;
     }
 
     /**
-     *
-     * @param loop 有环: ① 两个链表具有各自的环, 不相交 ② 两个链表拥有同一个环, 入环结点相同 ③ 两个链表拥有同一个环, 但是入环结点不同
-     * @return
+     * <h3>1. 两个链表都具有环, 但是不相交</h3>
+     * <h3>2. 两个链表都有环, 且在入环之前相交</h3>
+     * <h3>3. 两个链表都有环, 在入环之后相交</h3>
+     * <h3>注: 不存在一个链表有环, 另外一个链表没环, 两个链表还相交的情况</h3>
      */
-    private static ListNode intersectionLoopListLoop(ListNode l1, ListNode l2, String loop) {
-
-
+    private static ListNode getIntersectionNodeLoop(ListNode l1, ListNode l2) {
+        // 1. 如果没有环, 那么就调用刚才的方法去执行
+        ListNode firstNode = CircleList.detectCycle(l1);
+        ListNode secondNode = CircleList.detectCycle(l2);
+        if (firstNode == null && secondNode == null){
+            return getIntersectionNode(l1, l2);
+        }else if (firstNode != null && secondNode != null){
+            // 2. 两个链表都有环存在, 根据入环结点判断具体是哪种情况
+            // 2.1 如果两个入环结点是相同的, 那么认为两个链表在进入环之前就相交了, 和没有坏一样
+            if (firstNode == secondNode){
+                return getIntersectionNode(l1, l2);
+            }else{
+                // 2.2 如果两个入环结点都不相同, 那么就需要判断是剩下两种情况的哪一种
+                // 注: 做法就是让第一个入环结点一直遍历, 如果在回到自己之前能够遇见另外一个入环结点, 那么就是第三种情况
+                ListNode current = firstNode.next;
+                while (current != firstNode){
+                    // 这时候第一个入环结点或者第二个入环结点都可以视为相交结点
+                    if (current == secondNode)
+                        return firstNode;
+                    current = current.next;
+                }
+            }
+        }
+        // 3. 如果一个链表有环, 另外一个链表没环, 那么是不可能相交的
         return null;
     }
 

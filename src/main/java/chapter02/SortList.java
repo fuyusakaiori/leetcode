492ef0f908d8989825d0f24da85ebcb5367b462c
fuyusakaiori <670232228@qq.com>
2022-02-24 20:58:27 +0800

    排序链表：两种方式

--- src/main/java/chapter02/SortList.java
+++ src/main/java/chapter02/SortList.java
@@ -3,106 +3,149 @@ package chapter02;
 import utils.ListNode;
 import utils.RandomUtil;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 /**
  * <h2>排序链表</h2>
- * <p>问题描述: 就是对链表排序</p>
- * <p>进阶问题: 使用O(N*logN)时间复杂度完成</p>
  */
 public class SortList
 {
 
-    /**
-     * <p>这个题有两种思路可以解</p>
-     * <p>1. 最直接的就是将链表的结点全部存入数组中, 然后对数组进行排序</p>
-     * <p>2。第二种方法非常不好想, 是自底向上的归并排序 </p>
-     */
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        ListNode dummy = new ListNode(0);
+        ListNode node1 = new ListNode(4);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(1);
+        ListNode node4 = new ListNode(3);
 
+        dummy.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
 
+        sortListTop(dummy.next);
     }
 
-    public static ListNode sort(ListNode head){
-        // 排除额外情况
-        if(head == null)
-            return null;
+    /**
+     * <h3>思路: 直接使用排序工具类解决</h3>
+     */
+    public static ListNode sortListUtil(ListNode head){
+        List<ListNode> nodes = new LinkedList<>();
+        while (head != null){
+            nodes.add(head);
+            head = head.next;
+        }
+        // 注: 可以继续简化, 但是简化出来就看不出是个什么东西了
+        nodes.sort((node1, node2) -> node1.value - node2.value);
 
-        // 获取链表长度
-        int length = 0;
-        ListNode current = head;
-        while(current != null && ++length > 0)
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        for(ListNode node : nodes){
+            node.next = null;
+            current.next = node;
             current = current.next;
+        }
+        return dummy.next;
+    }
 
-        current = head;
-        ListNode h1;
-        ListNode h2;
-        ListNode previous = null;
-        ListNode next = null;
+    /**
+     * <h3>思路: 自己编写排序算法, 显然不如工具类的算法</h3>
+     * <h3>注: 主要目的是复习归并算法</h3>
+     * <h3>注: 如果使用数组存放计算中点就更容易, 如果直接使用链表合并就需要快慢指针找中点, 快慢指针找中点更快</h3>
+     * <h3>注: 合并 K 个升序链表不需要找中点</h3>
+     */
+    private static ListNode sortListTop(ListNode head) {
+        return fork(head, null);
+    }
+
+    private static ListNode fork(ListNode head, ListNode tail){
+        if(head.next == tail){
+            head.next = null;
+            return head;
+        }
+        ListNode mid = findMid(head, tail);
+        ListNode leftNode = fork(head, mid);
+        // 注: 因为找到的是第二个中点, 所以不能够传入中点的下一个结点作为头结点, 否则就造成左侧比右侧多一个结点的情况
+        ListNode rightNode = fork(mid, tail);
+        return mergeList(leftNode, rightNode);
+    }
+
+    private static ListNode findMid(ListNode head, ListNode tail){
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != tail && fast.next != tail){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    /**
+     * <h3>思路: 自底向上的归并排序</h3>
+     * <h3>1. 最初每个分区仅有一个结点, 然后两两合并成有两个结点的单元, 依次类推</h3>
+     * <h3>2. 每轮合并的次数取决于分区的数量</h3>
+     */
+    private static ListNode sortListDown(ListNode head){
+        int length = getLength(head);
         ListNode dummy = new ListNode(0, head);
-        // 初始每个分区长度仅为1
-        for(int subLength = 1;subLength < length;subLength<<=1){
+        ListNode current = dummy.next;
+        ListNode previous = dummy;
+        ListNode next = null;
+        for (int subLength = 1;subLength < length;subLength <<= 1){
+            // 每轮合并结束都需要重置前驱结点和当前结点
             previous = dummy;
             current = dummy.next;
-            // 分区两两合并
-            while(current != null){
-                // 记录第一个分区的头指针
-                h1 = current;
-                // 开始向后移动, 寻找第二个分区的头指针
-                for(int i = 1;i < subLength && current.next != null;i++)
+            while (current != null){
+                ListNode h1 = current;
+                for (int index = 1;index < subLength && current.next != null;index++)
                     current = current.next;
-                // 记录第二个分区的头指针
-                h2 = current.next;
-                // 断开两个链表之间的联系, 否则合并链表的时候没有结束
-                current.next = null;
-                // 更新当前结点
-                current = h2;
-                // 开始向后移动, 寻找第三个分区的头结点, 留着下次使用
-                for(int i = 1;i < subLength && current != null && current.next != null;i++)
+                ListNode t1 = current;
+                current = current.next;
+                ListNode h2 = current;
+                for (int index = 1;index < subLength && current != null && current.next != null;index++)
                     current = current.next;
-                // 再次断开两个链表之间的联系, 否者合并链表的时候没有结束
-                //
-                if(current != null){
-                    next = current.next;
-                    current.next = null;
+                ListNode t2 = current;
+                t1.next = null;
+                if(t2 != null){
+                    next = t2.next;
+                    t2.next = null;
                 }
-                // 合并链表
-                previous.next = merge(h1, h2);
-                while(previous.next != null)
-                    previous = previous.next;
-                // 重复上述过程
+                previous.next = mergeList(h1, h2);
+                while (previous.next != null) previous = previous.next;
                 current = next;
                 next = null;
             }
         }
-
         return dummy.next;
     }
 
-    private static ListNode merge(ListNode h1, ListNode h2) {
+    private static int getLength(ListNode head){
+        int length = 0;
+        while (head != null && ++length > 0){
+            head = head.next;
+        }
+        return length;
+    }
+
+    private static ListNode mergeList(ListNode l1, ListNode l2){
         ListNode dummy = new ListNode(0);
         ListNode current = dummy;
-        while(h1 != null && h2 != null){
-            if(h1.value < h2.value){
-                current.next = h1;
-                h1 = h1.next;
+        while (l1 != null && l2 != null){
+            if (l1.value <= l2.value){
+                current.next = l1;
+                l1 = l1.next;
             }else{
-                current.next = h2;
-                h2 = h2.next;
+                current.next = l2;
+                l2 = l2.next;
             }
-
             current = current.next;
         }
-        // 这里不使用循环是因为每个链表都是有序的, 所以没有必要
-        if(h1 != null)current.next = h1;
-        if(h2 != null)current.next = h2;
-
+        if (l1 != null) current.next = l1;
+        if (l2 != null) current.next = l2;
         return dummy.next;
     }
+
 
 }
 

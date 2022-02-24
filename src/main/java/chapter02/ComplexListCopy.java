401b1fe06f26ab5adece39c2673fb7b2441e5224
fuyusakaiori <670232228@qq.com>
2022-02-24 23:02:54 +0800

    复制带随机指针的链表

--- src/main/java/chapter02/ComplexListCopy.java
+++ src/main/java/chapter02/ComplexListCopy.java
@@ -8,11 +8,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * <h2>复杂链表复制</h2>
- * <p>问题描述</p>
- * <p>1. 每个结点具有额外的 random 指针</p>
- * <p>2. random 指针可以指向任何一个结点</p>
- * <p>两种方式核心思路都是采用克隆</p>
+ * <h2>复制带随机指针的链表</h2>
  */
 public class ComplexListCopy
 {
@@ -22,65 +18,58 @@ public class ComplexListCopy
     }
 
     /**
-     * 采用哈希表完成
-     * @param head 头结点
-     * @return 复制的链表
+     * <h3>思路: 哈希表实现</h3>
      */
     private static ListNode copyComplexListHash(ListNode head){
+        Map<ListNode, ListNode> map = new HashMap<>();
         ListNode current = head;
-        // 0. Key 代表的是原链表中的结点, Value 代表的是原链表结点的拷贝
-        HashMap<ListNode, ListNode> map = new HashMap<>();
-        // 1. 先对原链表的进行浅拷贝, 就是说不复制随机指针
+        // 1. 克隆结点
         while (current != null){
-            // 注意: 这里可以不进行 next 指针的拷贝也可以进行
-            map.put(current, new ListNode(current.value, current.next));
+            map.put(current, new ListNode(current.value));
             current = current.next;
         }
-        // 2. 开始对原链表的深拷贝
+        // 2. 建立 random、next 指针
         current = head;
-        while (current != null) {
+        while (current != null){
+            map.get(current).next = current.next;
             map.get(current).random = map.get(current.random);
             current = current.next;
         }
-
         return map.get(head);
     }
 
     /**
-     * 不借助额外空间完成
+     * <h3>思路: 原链表中插入克隆结点, 复制完成后分离</h3>
      */
     private static ListNode copyComplexList(ListNode head){
-        // 1. 遍历链表对每个结点进行复制, 然后链接在原结点的后面
         ListNode current = head;
+        // 1. 复制结点
         while (current != null){
             ListNode node = new ListNode(current.value);
             node.next = current.next;
             current.next = node;
-            current = node.next;
-        }
-        current = head;
-        // 2. 开始拷贝随机指针
-        while (current != null){
-            // 2.1 如果原结点就没有随机指针那么就不需要设置
-            if (current.random != null)
-                current.next.random = current.random.next;
             current = current.next.next;
         }
+        // 2. 建立 random 指针
+        current = head;
+        while (current != null){
+            ListNode copy = current.next;
+            copy.random = current.random != null ? current.random.next: null;
+            current = current.next.next;
+        }
+        // 3. 建立 next 指针
         ListNode dummy = new ListNode(0);
         ListNode copy = dummy;
         current = head;
-        // 3. 开始分离拷贝链表和原链表
         while (current != null){
-            // 3.1 复制链表链接拷贝结点
             copy.next = current.next;
-            // 3.2 原链表结点指向下下个结点
-            current.next = current.next.next;
-            // 3.3 更新
-            current = current.next;
             copy = copy.next;
+            current.next = current.next.next;
+            current = current.next;
         }
 
         return dummy.next;
+
     }
 
 }

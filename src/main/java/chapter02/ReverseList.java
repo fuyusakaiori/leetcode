6aa3ab27981b4be4253c78ee0dce03eff80d19c9
fuyusakaiori <670232228@qq.com>
2022-02-25 18:43:30 +0800

    反转链表

--- src/main/java/chapter02/ReverseList.java
+++ src/main/java/chapter02/ReverseList.java
@@ -4,9 +4,9 @@ import utils.ListNode;
 
 /**
  * <h2>反转链表</h2>
- * <p>1. 反转整个链表</p>
- * <p>2. 指定范围反转</p>
- * <p>3. K个一组反转</p>
+ * <h3>1.反转链表</h3>
+ * <h3>2.反转链表 II</h3>
+ * <h3>3.K个一组反转</h3>
  * <p>万能解法: 集合、数组、栈都是可以完成的</p>
  */
 public class ReverseList
@@ -17,12 +17,6 @@ public class ReverseList
     }
 
 
-    /**
-     * 反转整个链表
-     * <p>1. 可以使用递归的形式完成, 不过比较麻烦, 而且占用额外空间（系统栈）</p>
-     * <p>2. 可以使用头插法或者栈完成, 不过也是需要使用额外空间的</p>
-     * <p>3. 真正的不借助额外空间的做法实际上是原地反转</p>
-     */
     private static ListNode reverseAll(ListNode head){
         if(head == null || head.next == null)
             return head;
@@ -41,80 +35,72 @@ public class ReverseList
     /**
      * 反转指定范围内的部分链表
      */
-    private static ListNode reversePartition(ListNode head, int left, int right){
-        // 0. 空链表和元素唯一的链表是没有必要反转的
-        if(head == null || head.next == null)
-            return head;
-        // 1. 左右两个边界都是从一开始计数的, 不是从零开始的
+    public static ListNode reverseBetween(ListNode head, int left, int right) {
         int index = 1;
-        // 2. 反转链表常用的常量
-        ListNode previous = null;
+        ListNode dummy = new ListNode(0, head);
         ListNode current = head;
-        ListNode next = head.next;
-        // 3. 使用哑元避免之后的越界问题
-        // 3.1 主要是有可能反转的起始结点就是头结点, 而头结点之前没有任何结点
-        // 3.2 如果使用空间点之后就有各种各样的空指针异常, 所以使用哑元避免这种问题
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode leftNode = dummy;
-        ListNode rightNode = null;
-        while(index <= right){
-            // 4。记录左边界的前一个结点
-            leftNode = index == left - 1 ? current: leftNode;
-            // 5. 记录右边界
-            rightNode = index == right ? current: rightNode;
-            // 6. 到达左边界之后才开始反转
-            if(index > left)
-                current.next = previous;
+        ListNode previous = null;
+        ListNode next = null;
+        // 先到达需要反转的位置
+        while(index <= left){
+            dummy = index == left - 1 ? current: dummy;
             previous = current;
-            current = next;
-            next = next != null ? next.next : null;
+            current = current.next;
             index++;
         }
-        leftNode.next.next = current;
-        leftNode.next = rightNode;
+        // 开始反转
+        while(index++ <= right){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+        dummy.next.next = current;
+        dummy.next = previous;
+        return dummy.value != 0 ? head: dummy.next;
+    }
+
+    public static ListNode reverseKGroup(ListNode head, int k) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode tail = dummy;
+        ListNode previous = dummy;
+        ListNode current = head;
+        ListNode next = null;
+        while(current != null){
+            // 找到需要反转的区域, 也兼具查看剩下的链表是否需要反转的功能
+            for(int index = 0;index < k;index++){
+                tail = tail.next;
+                // 如果尾指针为空, 证明剩余部分不够
+                if(tail == null) return dummy.next;
+            }
+            // 记录下一部分链表的头结点
+            next = tail.next;
+            // 反转链表
+            ListNode[] nodes = reverse(current, tail);
+            // 接回原链表
+            current = nodes[0];
+            tail = nodes[1];
+            previous.next = current;
+            tail.next = next;
+            // 更新尾指针
+            previous = tail;
+            // 向后移动
+            current = next;
+        }
         return dummy.next;
     }
-
-    /**
-     * <p>K 个一组反转链表</p>
-     * <p>1. 没有任何优化的思路比较慢, 但是核心肯定是这样的</p>
-     */
-    private static ListNode reverse(ListNode head, int k){
-        if(head == null || head.next == null)
-            return head;
-        int index = 0;
-        int length = getLength(head);
-        int count = length / k;
+    // 反转链表, 并且返回头尾指针
+    public static ListNode[] reverse(ListNode head, ListNode tail){
         ListNode previous = null;
         ListNode current = head;
-        ListNode next = head.next;
-        ListNode tail = new ListNode(-1);
-        ListNode temp = null;
-        while(count > 0){
-            if(index != k){
-                temp = index == 0 ? current: temp;
-                current.next = previous;
-                previous = current;
-                current = next;
-                next = next != null ? next.next: null;
-                index++;
-            }else{
-                head = tail.value == -1 ? previous: head;
-                tail.next = previous;
-                tail = temp;
-                index = 0;
-                count--;
-            }
+        ListNode next = null;
+        ListNode end = tail.next;
+        while(current != end){
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
         }
-        tail.next = current;
-        return head;
-    }
-
-    private static int getLength(ListNode head) {
-        int length = 0;
-        ListNode current = head;
-        while(current != null && ++length > 0)current = current.next;
-        return length;
+        return new ListNode[]{tail, head};
     }
 }

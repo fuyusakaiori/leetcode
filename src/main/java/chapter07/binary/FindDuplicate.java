9bbc02ef3ee804b5f84950ec46d20dab7ca11c75
fuyusakaiori <670232228@qq.com>
2022-05-12 17:11:26 +0800

    寻找重复数

--- src/main/java/chapter07/binary/FindDuplicate.java
+++ src/main/java/chapter07/binary/FindDuplicate.java
@@ -9,57 +9,50 @@ package chapter07.binary;
 public class FindDuplicate {
 
     /**
-     * <h3>思路: 寻找重复数</h3>
-     * <h3>1. 这个题的思路非常巧妙, 换句话说就是不好想</h3>
-     * <h3>2. 二分查找</h3>
-     * <h3>3. 快慢指针</h3>
-     */
-    private static int findDuplicate(int[] nums){
-        return fastSlowPointer(nums);
-    }
-
-    /**
      * <h3>二分查找</h3>
-     * <h3>1. 首先数组中的元素都是无序的, 所以肯定是不可能直接使用二分查找的, 所以考虑构建一个可以二分的东西</h3>
-     * <h3>2. 我们就需要维护一个 cnt 数组, 记录数组中比 nums[i] 小的元素的个数 </h3>
-     * <h3>例: {1, 3, 4, 2, 2}, cnt: <=1 1, <=2 3, <=3 4, <=4 5</h3>
-     * <h3>3. 然后再仔细观察就会发现</h3>
-     * <h3>3.1 如果 nums[i] < target, 那么 cnt <= target</h3>
-     * <h3>3.2 如果 nums[i] >= target, 那么 cnt > target</h3>
-     * <h3>注: 这个条件反过来也是成立的, 至于存在的细节, 就自己去力扣的文章中看了</h3>
-     * <h3>4. 此时就可以使用二分法了</h3>
-     * <h3>4.1 如果 cnt > target, 那么证明当前值可能大于等于目标值, 需要记录, 然后向左移动右指针</h3>
-     * <h3>4.2 如果 cnt < target, 那么就证明当前值是小于目标值的, 不需要记录, 然后向右移动左指针</h3>
-     * <h3>5. 但是依然不可能直接在原数组上二分, 这个是不现实的, 要求空间复杂度为 O(1), 在 cnt 上二分也不行</h3>
-     * <h3>6. 仔细观察, 你会发现数组中的值和索引是有关系的 => n+1个数, 数组的值: 1 ~ n, 索引 0 ~ n</h3>
-     * <h3>7. 也就是我们可以在索引上进行二分, 只要 cnt 大于等于自己, 就向左移, 左边的数一定比自己小</h3>
-     * <h3>8. 也就相当于在 cnt 上向左移动</h3>
-     * <h3>注: 有些细节这里没有提及, 但是可以直接被处理</h3>
+     * <h3>1. 在没有任何提示的情况下这个题基本很难想到二分查找的方式, 这是因为原数组本身就是无序的</h3>
+     * <h3>2. 如果使用排序让数组有序, 二分查找也就失去了意义, 所以想要在无序数组中使用二分就要构造有序数组</h3>
+     * <h3>3. 所以这个构造有序数组的过程就是非常玄学的事情, 有点靠灵感或者猜测的意思</h3>
+     * <h3>4. 这个题提到数组中所有的元素都是在 [1,n] 范围内的, 也就是说数组中的数字不是混乱的, 是可以预测的</h3>
+     * <h3>5. 根据这个特性就可以统计小于等于 nums[i] 的数有多少个, 并且暂时保存在 cnt 数组中</h3>
+     * <h3>5.1 {1, 3, 4, 2, 2}, cnt: <=1 1, <=2 3, <=3 4, <=4 5</h3>
+     * <h3>5.2 通过统计小于等于 nums[i] 的数得到的 cnt 数组就具有单调性了, 这样就可以使用二分查找</h3>
+     * <h3>6. 二分查找的规则也是非常不好想到的, 这里假设重复的数是 target</h3>
+     * <h3>6.1 存在这样的关系: 如果没有重复的数字, 显然 cnt[i] == nums[i], 因为每个数字都只出现一次</h3>
+     * <h3>6.2 如果 nums[i] < target, 那么对应 cnt[i] <= nums[i], 因为其余的数要么出现一次要么没有出现</h3>
+     * <h3>6.3 如果 nums[i] > target, 那么 cnt[i] >= nums[i], 这是因为包含目标值在内, 所以次数肯定大</h3>
+     * <h3>7. 在题目中还要求空间复杂度为 O(1), 所以肯定不能用额外数组保存, 所以只能够每次都去统计</h3>
+     * <h3>8. 索引恰好可以表达数组中的数字, 所以就可以直接对索引进行统计, 而不是对数组元素进行统计</h3>
      */
-    private static int binarySearch(int[] nums){
-        int result = -1;
+    private static int findDuplicate1(int[] nums){
+        int duplicate = 0;
         int left = 0, right = nums.length - 1;
         while (left <= right){
-            int mid = (right + left) >> 1;
-            int count = getCount(nums, mid);
-            if (count > mid){
-                result = mid;
+            // 1. 获取需要进行统计的数字
+            int mid = (left + right) >> 1;
+            // 2. 获取小于等于这个数字的个数
+            int cnt = getCount(nums, mid);
+            // 3. 判断个数和数字的大小关系:
+            if (cnt > mid){
+                // 4. 如果个数比数字大, 那么现在肯定偏向右侧, 向左侧移动, 但是这里需要记录结果
+                duplicate = mid;
                 right = mid - 1;
-            }
-            if (count < mid)
+            }else{
+                // 5. 如果个数比较小, 那么现在肯定偏向左侧, 向右侧移动
                 left = mid + 1;
+            }
         }
-        return result;
+        return duplicate;
     }
 
     private static int getCount(int[] nums, int mid){
-        int count = 0;
-        for(int index = 0;index < nums.length;index++){
-            // 注: 这里使用的是索引替代数组中的值
-            if(nums[index] <= mid) count++;
+        int cnt = 0;
+        for (int num : nums){
+            cnt += num <= mid ? 1: 0;
         }
-        return count;
+        return cnt;
     }
+
 
     /**
      * <h3>思路: 快慢指针</h3>
@@ -69,21 +62,22 @@ public class FindDuplicate {
      * <h3>数组的索引: {0, 1, 2, 3, 4}</h3>
      * <h3>3. 那么可以这么考虑: 0 -> 1 -> 3 -> 2 -> 4 -> 2</h3>
      * <h3>4. 形成了环状链表, 所以可以使用快慢指针来找入环的结点, 也就是重复的结点</h3>
+     * <h3>注: 为什么入环结点恰好就是重复的结点?</h3>
      */
-    private static int fastSlowPointer(int[] nums){
-        int slow = 0;
-        int fast = 0;
-        do {
-            slow = nums[slow];
+    private static int findDuplicate2(int[] nums){
+        // 1. 快慢指针
+        int fast = 0, slow = 0;
+        // 2. 第一次相遇
+        do{
             fast = nums[nums[fast]];
-        }while (slow != fast);
-
+            slow = nums[slow];
+        }while (fast != slow);
+        // 3. 第二次相遇
         fast = 0;
         while (fast != slow){
             fast = nums[fast];
             slow = nums[slow];
         }
-        // 注: 这里返回的是指针, 而不是数组中的值
         return slow;
     }
 

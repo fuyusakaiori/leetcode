7c35ba69fc6153dceeef845b407dd77efc5066ee
fuyusakaiori <670232228@qq.com>
2022-08-09 22:28:24 +0800

    分隔链表

--- leetcode-go/leetcode/chapter02/partitionlist.go
+++ leetcode-go/leetcode/chapter02/partitionlist.go
@@ -1,6 +1,7 @@
 package chapter02
 
-func Partition(head *ListNode, target int) *ListNode {
+// 分隔链表
+func partition(head *ListNode, target int) *ListNode {
 	// 1. 双指针
 	bigHead, smallHead := &ListNode{0, nil}, &ListNode{0, nil}
 	bigTail, smallTail := bigHead, smallHead
@@ -20,4 +21,36 @@ func Partition(head *ListNode, target int) *ListNode {
 	bigTail.Next = nil
 	smallTail.Next = bigHead.Next
 	return smallHead.Next
+}
+
+// 分隔链表
+func splitListToParts(head *ListNode, k int) []*ListNode {
+	if head == nil {
+		return make([]*ListNode, k)
+	}
+	// 1. 获取长度
+	length := getLength(head)
+	// 2. 分配链表数组
+	lists := make([]*ListNode, k)
+	// 3. 分隔链表
+	groupIndex := 0
+	current := head
+	for groupIndex < k && current != nil {
+		// 4. 记录每组链表的头结点
+		start := current
+		// 5. 计算每组大小
+		groupSize := (length + (k - groupIndex) - 1) / (k - groupIndex)
+		// 6. 减去每组大小
+		length -= groupSize
+		for groupSize > 1 {
+			current = current.Next
+			groupSize--
+		}
+		next := current.Next
+		current.Next = nil
+		lists[groupIndex] = start
+		current = next
+		groupIndex++
+	}
+	return lists
 }

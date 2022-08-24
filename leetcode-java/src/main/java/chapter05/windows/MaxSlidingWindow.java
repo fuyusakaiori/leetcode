b8edf5d28837296ecfe687cb306df5eabfa62aab
fuyusakaiori <670232228@qq.com>
2022-08-24 21:36:41 +0800

    滑动窗口最大值

--- leetcode-java/src/main/java/chapter05/windows/MaxSlidingWindow.java
+++ leetcode-java/src/main/java/chapter05/windows/MaxSlidingWindow.java
@@ -1,8 +1,6 @@
 package chapter05.windows;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.PriorityQueue;
+import java.util.*;
 
 /**
  * <h3>滑动窗口最大值</h3>
@@ -12,21 +10,26 @@ public class MaxSlidingWindow {
     /**
      * <h3>优先权队列</h3>
      */
-    public static int[] maxSlidingWindow1(int[] nums, int k) {
-        int[] maxValue = new int[nums.length - k + 1];
-        // 1. 滑动窗口
-        PriorityQueue<int[]> queue = new PriorityQueue<>((a, b) -> b[1] - a[1]);
-        // 2. 初始化
-        for (int idx = 0;idx < k;idx++){
-            queue.offer(new int[]{idx, nums[idx]});
+    public static int[] maxSlidingWindow1(int[] nums, int cnt) {
+        // 1. 最大值数组的长度
+        int[] maxValue = new int[nums.length - cnt + 1];
+        // 2. 滑动窗口: 优先权队列 记录数组的索引并依据元素大小排序 -> 记录索引的目的是判断最大值是否已经在滑动窗口之外
+        PriorityQueue<int[]> window = new PriorityQueue<>((a, b) -> b[1] - a[1]);
+        // 3. 初始化窗口大小: 窗口大小固定的最好先初始化, 不要合成单个循环
+        for(int idx = 0;idx < cnt;idx++){
+            window.offer(new int[]{idx, nums[idx]});
         }
-        // 3. 移动窗口
-        maxValue[0] = queue.peek() != null ? queue.peek()[1] : 0;
-        for(int idx = k;idx < nums.length;idx++){
-            queue.offer(new int[]{idx, nums[idx]});
-            while(!queue.isEmpty() && queue.peek()[0] <= idx - k)
-                queue.poll();
-            maxValue[idx - k + 1] = queue.peek() != null ? queue.peek()[1] : 0;
+        // 4. 获取最大值
+        maxValue[0] = Objects.requireNonNull(window.peek())[1];
+        // 5. 移动窗口
+        for(int idx = cnt;idx < nums.length;){
+            // 6. 先将新元素加入到优先权队列中
+            window.offer(new int[]{idx, nums[idx++]});
+            // 7. 更新优先权队列中的最大值
+            while(!window.isEmpty() && window.peek()[0] < idx - cnt)
+                window.poll();
+            // 8. 更新最值
+            maxValue[idx - cnt] = Objects.requireNonNull(window.peek())[1];
         }
         return maxValue;
     }
@@ -34,26 +37,21 @@ public class MaxSlidingWindow {
     /**
      * <h3>单调队列</h3>
      */
-    public static int[] maxSlidingWindow2(int[] nums, int k){
-        // 1。 滑动窗口必然需要准备的变量
-        int left = 0, right = 0;
-        int[] maxArray = new int[nums.length - k + 1];
-        // 2. 大多数的滑动窗口都需要相应的数据结构
-        Deque<Integer> window = new ArrayDeque<>();
-        // 3. 开始移动滑动窗口: 这里的滑动窗口大小是固定的, 之前有些题窗口大小不是固定的
-        while (right < nums.length){
-            // 4. 确保队列是单调递减的, 相当于单调栈
-            while (!window.isEmpty() && nums[window.peekLast()] < nums[right])
+    public static int[] maxSlidingWindow2(int[] nums, int cnt){
+        // 1. 最大值数组
+        int[] maxValue = new int[nums.length - cnt + 1];
+        // 2. 滑动窗口
+        Deque<Integer> window = new LinkedList<>();
+        // 3. 移动窗口
+        for (int idx = 0; idx < nums.length;) {
+            while (!window.isEmpty() && nums[window.peekLast()] < nums[idx])
                 window.pollLast();
-            // 5. 确保单调性之后再将当前元素入队
-            window.offerFirst(right++);
-            // 6. 确保当前的最大值还在窗口中
-            if (window.peekFirst() < left)
+            window.offerLast(idx++);
+            if (window.peekFirst() < idx - cnt)
                 window.pollFirst();
-            // 7. 窗口大小满足之后就可以开始移动窗口, 填充最大值了
-            if (right >= k)
-                maxArray[left] = nums[window.pollFirst()];
+            if (idx >= cnt)
+                maxValue[idx - cnt] = nums[window.peekFirst()];
         }
-        return maxArray;
+        return maxValue;
     }
 }

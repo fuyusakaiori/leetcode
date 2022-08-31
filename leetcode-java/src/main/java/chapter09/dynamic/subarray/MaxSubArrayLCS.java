701f47a389854ee7039cf7440ba329715656b3fc
fuyusakaiori <670232228@qq.com>
2022-08-31 11:17:49 +0800

    最长上升子数组

---
+++ leetcode-java/src/main/java/chapter09/dynamic/subarray/MaxSubArrayLCS.java
@@ -0,0 +1,45 @@
+package chapter09.dynamic.subarray;
+
+/**
+ * <h3>最长上升子数组</h3>
+ */
+public class MaxSubArrayLCS {
+
+
+    /**
+     * <h3>动态规划: 从左到右</h3>
+     */
+    public static int findLengthOfLCIS1(int[] nums){
+        // 1. 表结构: dp[i] 表示以 nums[i] 结尾的最大升序子数组
+        int[] dp = new int[nums.length];
+        // 2. basecase
+        dp[0] = 1;
+        // 3. 填表
+        int maxLength = dp[0];
+        for(int idx = 1;idx < nums.length;idx++){
+            dp[idx] = nums[idx] <= nums[idx - 1] ? 1: dp[idx - 1] + 1;
+            maxLength = Math.max(maxLength, dp[idx]);
+        }
+        return maxLength;
+    }
+
+    public int findLengthOfLCIS2(int[] nums) {
+        int maxLength = 0;
+        for(int idx = 0;idx < nums.length;idx++){
+            maxLength = Math.max(maxLength, findLengthOfLCIS2(idx, nums));
+        }
+        return maxLength;
+    }
+
+    /**
+     * <h3>递归</h3>
+     */
+    public int findLengthOfLCIS2(int idx, int[] nums){
+        if(idx == nums.length)
+            return 1;
+        if(idx > 0 && nums[idx - 1] < nums[idx])
+            return findLengthOfLCIS2(idx + 1, nums) + 1;
+        return 1;
+    }
+
+}

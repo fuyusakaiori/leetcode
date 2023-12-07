35ea31437942fb1bded47b3824890943574f4acf
zhouhang.shen <zhouhang.shen@jiduauto.com>
2023-12-07 14:15:47 +0800

    两数相加

---
+++ leetcode-go/chapter02/calculate_number.go
@@ -0,0 +1,61 @@
+package main
+
+
+// 两数相加
+func addTwoNumbers(first *ListNode, second *ListNode) *ListNode {
+	carry, dummy := 0, new(ListNode)
+	tail := dummy
+	for first != nil || second != nil {
+		firstVal, secondVal := 0, 0
+		if first != nil {
+			firstVal = first.Val
+			first = first.Next
+		}
+		if second != nil {
+			secondVal = second.Val
+			second = second.Next
+		}
+		result := (firstVal + secondVal + carry) % 10
+		carry = (firstVal + secondVal + carry) / 10
+		node := &ListNode{Val: result}
+		tail.Next = node
+		tail = tail.Next
+	}
+	if carry != 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return dummy.Next
+}
+
+// 两数相加 II
+func addTwoNumbersV2(first *ListNode, second *ListNode) *ListNode {
+	// 反转链表
+	first, second = reverseListV1(first), reverseListV1(second)
+	// 模拟计算
+	carry, dummy := 0, new(ListNode)
+	tail := dummy
+	for first != nil || second != nil {
+		firstVal, secondVal := 0, 0
+		if first != nil {
+			firstVal = first.Val
+			first = first.Next
+		}
+		if second != nil {
+			secondVal = second.Val
+			second = second.Next
+		}
+		result := (firstVal + secondVal + carry) % 10
+		carry = (firstVal + secondVal + carry) / 10
+		node := &ListNode{Val: result}
+		tail.Next = node
+		tail = tail.Next
+	}
+	if carry != 0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return reverseListV1(dummy.Next)
+}
+
+func main() {
+
+}
